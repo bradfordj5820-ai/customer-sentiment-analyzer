f5814bd67da399b2d2fbaf757907a13c1f932f30
bradfordj5820-ai <bradfordj5820@gmail.com>
2026-09-22 09:59:53 -0500

    Change customer data file path to relative
    Updated the file path for customer data to a relative path.

--- customer_feedback.py
+++ customer_feedback.py
@@ -14,7 +14,8 @@ from dataclasses import dataclass
 #import matplotlib.pyplot as plt
 
 # Load and Merge Data
-customers = pd.read_csv('/content/drive/MyDrive/Python Data Files/Customer_Data.csv')
+#customers = pd.read_csv('/content/drive/MyDrive/Python Data Files/Customer_Data.csv')
+customers = pd.read_csv('Customer_Data.csv')
 feedback = pd.read_csv('/content/drive/MyDrive/Python Data Files/customer_feedback.csv')
 merged_data = pd.merge(customers, feedback, on='name')
 
